04ff21abfc7e724d1b4fb185a05f4c2cdb4b5074
Vojislav Trujkic <voizlav@gmail.com>
2023-02-06 17:56:27 +0100

    Add eslint config

---
+++ cli/.eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  env: {
+    browser: true,
+    commonjs: true,
+    es2021: true,
+  },
+  extends: ["prettier"],
+  plugins: ["prettier"],
+  rules: {
+    "prettier/prettier": ["error"],
+  },
+};
