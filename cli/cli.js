7c059e1d9419c0395fd28d07ba675392795bade5
Vojislav Trujkic <voizlav@gmail.com>
2023-02-06 18:42:44 +0100

    Add name and number validation

--- cli/cli.js
+++ cli/cli.js
@@ -6,3 +6,26 @@ if (!process.argv.length === 3 || !process.argv.length === 5) {
   console.error("Usage: node cli.js password [name] [number]");
   process.exit(1);
 }
+
+/* Validate the input when the name and number have been provided */
+if (process.argv.length === 5) {
+  if (process.argv[3].length > 30 || process.argv[3].length < 3) {
+    console.error("Name length out of scope");
+    process.exit(1);
+  }
+
+  if (process.argv[4].length > 15 || process.argv[4].length < 3) {
+    console.error("Number length out of scope");
+    process.exit(1);
+  }
+
+  if (!/^\p{L}+(\s\p{L}+)*$/u.test(process.argv[3])) {
+    console.error("Invalid name");
+    process.exit(1);
+  }
+
+  if (!/^\d+(?:-\d+)?$/.test(process.argv[4])) {
+    console.error("Invalid number");
+    process.exit(1);
+  }
+}
