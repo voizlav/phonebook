981af142651eb9f95d062955a44f28efcc278100
Vojislav Trujkic <voizlav@gmail.com>
2023-02-06 18:42:00 +0100

    Add check for agrs

--- cli/cli.js
+++ cli/cli.js
@@ -1,1 +1,8 @@
-// TODO
+require("dotenv").config();
+const mongoose = require("mongoose");
+
+/* Check if the password is given */
+if (!process.argv.length === 3 || !process.argv.length === 5) {
+  console.error("Usage: node cli.js password [name] [number]");
+  process.exit(1);
+}
