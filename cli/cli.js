9daeb6ed6362a0440f1f851185c105f346fd2d93
Vojislav Trujkic <voizlav@gmail.com>
2023-02-06 18:55:17 +0100

    Update password checking

--- cli/cli.js
+++ cli/cli.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 const mongoose = require("mongoose");
 
 /* Check if the password is given */
-if (!process.argv.length === 3 || !process.argv.length === 5) {
+if (process.argv.length !== 3 || process.argv.length !== 5) {
   console.error("Usage: node cli.js password [name] [number]");
   process.exit(1);
 }
